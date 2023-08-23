025b2c89747f404b2aaf26062cf0c99174902de7
Mikhail Tvarkovsky <mtvarkovsky@gmail.com>
2023-08-23 09:52:36 +0200

    Add FileProcessor

---
+++ pkg/files/processor.go
@@ -0,0 +1,233 @@
+package files
+
+import (
+	"context"
+	"encoding/csv"
+	"fmt"
+	"github.com/shopspring/decimal"
+	"go.uber.org/zap"
+	"io"
+	"os"
+	"prices/pkg/config"
+	"prices/pkg/models"
+	"prices/pkg/repository"
+	"sync"
+	"time"
+)
+
+type (
+	LineProcessor interface {
+		ProcessLines()
+	}
+
+	FileProcessor interface {
+		ProcessFiles()
+	}
+
+	Processor interface {
+		LineProcessor
+		FileProcessor
+		Process()
+	}
+
+	processor struct {
+		ctx     context.Context
+		wg      *sync.WaitGroup
+		wgRead  *sync.WaitGroup
+		wgWrite *sync.WaitGroup
+		config  *config.FileProcessor
+		data    chan []*models.Price
+		files   FileQueue
+		repo    repository.Prices
+		logger  *zap.Logger
+		stop    <-chan bool
+	}
+)
+
+func NewProcessor(
+	ctx context.Context,
+	wg *sync.WaitGroup,
+	config *config.FileProcessor,
+	files FileQueue,
+	repo repository.Prices,
+	logger *zap.Logger,
+	stop <-chan bool,
+) Processor {
+	log := logger.Named("FileProcessor")
+	wgRead := &sync.WaitGroup{}
+	wgWrite := &sync.WaitGroup{}
+	p := &processor{
+		wg:      wg,
+		wgRead:  wgRead,
+		wgWrite: wgWrite,
+		ctx:     ctx,
+		config:  config,
+		data:    make(chan []*models.Price, config.DataBatchQueueSize),
+		files:   files,
+		repo:    repo,
+		logger:  log,
+		stop:    stop,
+	}
+	return p
+}
+
+func (p *processor) Process() {
+	p.wg.Add(1)
+	if p.config.ImportByLines {
+		p.ProcessLines()
+	} else {
+		p.ProcessFiles()
+	}
+	<-p.stop
+	p.logger.Sugar().Info("stop processing files")
+	p.wgRead.Wait()
+	close(p.data)
+	p.wgWrite.Wait()
+	p.wg.Done()
+}
+
+func (p *processor) ProcessLines() {
+	p.logger.Sugar().Info("start processing files by lines")
+	p.wgRead.Add(1)
+	go p.readFilesByLines()
+	for i := 0; i < p.config.WorkersCount; i++ {
+		p.wgWrite.Add(1)
+		go p.saveLines()
+	}
+}
+
+func (p *processor) saveLines() {
+	defer p.wgWrite.Done()
+	p.logger.Sugar().Info("start processing worker")
+	for prices := range p.data {
+		err := p.repo.Create(p.ctx, prices)
+		if err != nil {
+			p.logger.Sugar().Errorf("worker unable to proces data item: (%s)", err.Error())
+		}
+	}
+	p.logger.Sugar().Info("stop processing worker")
+}
+
+func (p *processor) readFilesByLines() {
+	defer p.wgRead.Done()
+	p.logger.Sugar().Info("start reading files")
+	data, err := p.files.Data()
+	if err != nil {
+		p.logger.Sugar().Infof("can't to get file queue data: (%s)", err.Error())
+		return
+	}
+	for file := range data {
+		p.wgRead.Add(1)
+		go p.readFileByLines(file)
+	}
+	p.logger.Sugar().Info("stop reading files")
+}
+
+func (p *processor) readFileByLines(file File) {
+	defer p.wgRead.Done()
+	p.logger.Sugar().Infof("start reading file=%s", file)
+	f, err := os.Open(file.Path)
+	if err != nil {
+		p.logger.Sugar().Errorf("can't open file=%s: (%s)", file, err.Error())
+	}
+	reader := csv.NewReader(f)
+	var prices []*models.Price
+	for {
+		line, err := reader.Read()
+		if err != nil {
+			if err == io.EOF {
+				p.logger.Sugar().Infof("done reading file=%s", file)
+				_ = f.Close()
+				break
+			}
+			p.logger.Sugar().Errorf("can't read file=%s data: (%s)", file, err.Error())
+			return
+		}
+		if price := p.toPrice(file.Path, line); price != nil {
+			prices = append(prices, price)
+			if len(prices) == p.config.DataBatchSize {
+				p.logger.Sugar().Infof("send file=%s data batch to processing", file)
+				p.data <- prices
+				prices = nil
+			}
+		}
+	}
+	//go func() {
+	//	p.logger.Sugar().Infof("try remove processed file=%s", file)
+	//	if err := os.Remove(file.Path); err != nil {
+	//		p.logger.Sugar().Errorf("can't remove processed file=%s: (%s)", file, err.Error())
+	//	}
+	//	p.logger.Sugar().Infof("removed processed file=%s", file)
+	//}()
+	//p.wg.Done()
+}
+
+func (p *processor) toPrice(path string, line []string) *models.Price {
+	price := &models.Price{}
+	if len(line) > 3 {
+		p.logger.Sugar().Errorf("bad file=%s format, only 3 columns expected", path)
+		return nil
+	}
+
+	id, priceData, expirationDate := line[0], line[1], line[2]
+
+	price.ID = id
+	parsedPrice, err := p.parsePriceData(priceData)
+	if err != nil {
+		p.logger.Sugar().Errorf("bad file=%s data, cant parse price: (%s)", path, err.Error())
+		return nil
+	}
+	price.Price = *parsedPrice
+	expDate, err := p.parseExpirationDate(expirationDate)
+	if err != nil {
+		p.logger.Sugar().Errorf("bad file=%s data, cant parse expirationDate: (%s)", path, err.Error())
+		return nil
+	}
+	price.ExpirationDate = *expDate
+	return price
+}
+
+func (p *processor) parsePriceData(priceData string) (*decimal.Decimal, error) {
+	price, err := decimal.NewFromString(priceData)
+	if err != nil {
+		return nil, fmt.Errorf("can't parse price data=%s as a floating point number", priceData)
+	}
+	return &price, nil
+}
+
+func (p *processor) parseExpirationDate(expirationDate string) (*time.Time, error) {
+	expDate, err := time.Parse("2006-01-02 15:04:05 -0700 MST", expirationDate)
+	if err != nil {
+		return nil, fmt.Errorf("can't parse expiration date=%s as a timestamp", expirationDate)
+	}
+	return &expDate, nil
+}
+
+func (p *processor) ProcessFiles() {
+	p.logger.Sugar().Info("start processing files")
+	for i := 0; i < p.config.WorkersCount; i++ {
+		p.wgWrite.Add(1)
+		go p.saveFiles()
+	}
+	//p.logger.Sugar().Info("stop reading files")
+}
+
+func (p *processor) saveFiles() {
+	p.logger.Info("start save files worker")
+	defer p.wgWrite.Done()
+	data, err := p.files.Data()
+	if err != nil {
+		p.logger.Sugar().Infof("can't to get file queue data: (%s)", err.Error())
+		return
+	}
+	for file := range data {
+		p.logger.Sugar().Infof("save file=%s to storage", file)
+		//p.wgInternal.Add(1)
+		err := p.repo.ImportFile(p.ctx, file.Path)
+		if err != nil {
+			p.logger.Sugar().Errorf("worker unable to proces file=%s: (%s)", file, err.Error())
+		}
+		//p.wgInternal.Done()
+	}
+	p.logger.Info("stop save files worker")
+}
---
+++ pkg/files/processor_test.go
@@ -0,0 +1,338 @@
+package files
+
+import (
+	"context"
+	"encoding/csv"
+	"fmt"
+	"github.com/golang/mock/gomock"
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"os"
+	"prices/pkg/config"
+	"prices/pkg/models"
+	"prices/pkg/repository"
+	"prices/pkg/testutils"
+	"sync"
+	"testing"
+	"time"
+)
+
+func newTestLineProcessor(t *testing.T) (*processor, chan bool) {
+	dir, err := os.MkdirTemp("", "")
+	assert.NoError(t, err)
+
+	files := NewFileQueueInMem(0)
+
+	wg := &sync.WaitGroup{}
+	log := zap.NewNop()
+
+	cfg := &config.FileProcessor{
+		FilesDir:           dir,
+		WorkersCount:       2,
+		DataBatchSize:      1,
+		DataBatchQueueSize: 1,
+		ImportByLines:      true,
+	}
+
+	ctx := context.Background()
+
+	ctrl := gomock.NewController(t)
+
+	repo := repository.NewMockPrices(ctrl)
+
+	stop := make(chan bool)
+
+	prcssr := NewProcessor(ctx, wg, cfg, files, repo, log, stop)
+
+	p := prcssr.(*processor)
+	return p, stop
+}
+
+func newTestFileProcessor(t *testing.T) (*processor, chan bool) {
+	dir, err := os.MkdirTemp("", "")
+	assert.NoError(t, err)
+
+	files := NewFileQueueInMem(0)
+
+	wg := &sync.WaitGroup{}
+	log := zap.NewNop()
+
+	cfg := &config.FileProcessor{
+		FilesDir:           dir,
+		WorkersCount:       2,
+		DataBatchSize:      1,
+		DataBatchQueueSize: 1,
+		ImportByLines:      false,
+	}
+
+	ctx := context.Background()
+
+	ctrl := gomock.NewController(t)
+
+	repo := repository.NewMockPrices(ctrl)
+
+	stop := make(chan bool)
+
+	prcssr := NewProcessor(ctx, wg, cfg, files, repo, log, stop)
+
+	p := prcssr.(*processor)
+	return p, stop
+}
+
+func TestProcessor_ParsePriceData(t *testing.T) {
+	prcssr, _ := newTestFileProcessor(t)
+
+	priceData := "3333.3333"
+
+	expected, err := decimal.NewFromString(priceData)
+	assert.NoError(t, err)
+
+	res, err := prcssr.parsePriceData(priceData)
+	assert.NoError(t, err)
+
+	assert.Equal(t, &expected, res)
+}
+
+func TestProcessor_ParseExpirationDate(t *testing.T) {
+	prcssr, _ := newTestFileProcessor(t)
+
+	expirationDate := "2023-08-25 10:42:33 +0200 CEST"
+
+	expected, err := time.Parse("2006-01-02 15:04:05 -0700 MST", expirationDate)
+	assert.NoError(t, err)
+
+	res, err := prcssr.parseExpirationDate(expirationDate)
+	assert.NoError(t, err)
+
+	assert.Equal(t, &expected, res)
+}
+
+func TestProcessor_ToPrice(t *testing.T) {
+	prcssr, _ := newTestFileProcessor(t)
+
+	line := []string{
+		"d65d3cba-40c7-11ee-afc6-a45e60d0762b",
+		"666.2111109",
+		"2023-08-23 10:42:33 +0200 CEST",
+	}
+	path := "test"
+
+	price, err := decimal.NewFromString(line[1])
+	assert.NoError(t, err)
+
+	expirationDate, err := time.Parse("2006-01-02 15:04:05 -0700 MST", line[2])
+	assert.NoError(t, err)
+
+	expected := &models.Price{
+		ID:             line[0],
+		Price:          price,
+		ExpirationDate: expirationDate,
+	}
+
+	res := prcssr.toPrice(path, line)
+	assert.Equal(t, *expected, *res)
+}
+
+func TestProcessor_ReadFileByLines(t *testing.T) {
+	prcssr, _ := newTestLineProcessor(t)
+	dir := prcssr.config.FilesDir
+
+	data := prcssr.data
+
+	testutils.GenerateTestData(2, dir)
+
+	entries, err := os.ReadDir(dir)
+	assert.NoError(t, err)
+	assert.Len(t, entries, 1)
+
+	entry := entries[0]
+
+	file := File{
+		Path: fmt.Sprintf("%s/%s", dir, entry.Name()),
+		Name: entry.Name(),
+	}
+
+	f, err := os.Open(file.Path)
+	assert.NoError(t, err)
+
+	reader := csv.NewReader(f)
+	lines, err := reader.ReadAll()
+	assert.NoError(t, err)
+
+	f.Close()
+
+	prcssr.wgRead.Add(1)
+	go prcssr.readFileByLines(file)
+
+	line1 := prcssr.toPrice(file.Path, lines[0])
+	assert.Equal(t, []*models.Price{line1}, <-data)
+
+	line2 := prcssr.toPrice(file.Path, lines[1])
+	assert.Equal(t, []*models.Price{line2}, <-data)
+}
+
+func TestProcessor_SaveLines(t *testing.T) {
+	prcssr, _ := newTestLineProcessor(t)
+
+	data := prcssr.data
+	repo := prcssr.repo.(*repository.MockPrices)
+
+	priceData1, err := decimal.NewFromString("2109.555555")
+	assert.NoError(t, err)
+	expirationDate1, err := time.Parse("2006-01-02 15:04:05 -0700 MST", "2023-08-23 16:32:48 +0200 CEST")
+	assert.NoError(t, err)
+	price1 := &models.Price{
+		ID:             "999f6c3c-402f-11ee-9150-a45e60d0762b",
+		Price:          priceData1,
+		ExpirationDate: expirationDate1,
+	}
+
+	priceData2, err := decimal.NewFromString("1776.1777776")
+	assert.NoError(t, err)
+	expirationDate2, err := time.Parse("2006-01-02 15:04:05 -0700 MST", "2023-08-25 16:32:48 +0200 CEST")
+	assert.NoError(t, err)
+	price2 := &models.Price{
+		ID:             "999f6c46-402f-11ee-9150-a45e60d0762b",
+		Price:          priceData2,
+		ExpirationDate: expirationDate2,
+	}
+
+	prices := [][]*models.Price{
+		{
+			price1,
+		},
+		{
+			price2,
+		},
+	}
+
+	prcssr.wgWrite.Add(1)
+	go prcssr.saveLines()
+
+	repo.EXPECT().Create(prcssr.ctx, prices[0]).Return(nil)
+	repo.EXPECT().Create(prcssr.ctx, prices[1]).Return(nil)
+
+	data <- prices[0]
+	data <- prices[1]
+
+	close(data)
+
+	prcssr.wgWrite.Wait()
+}
+
+func TestProcessor_SaveFiles(t *testing.T) {
+	prcssr, _ := newTestFileProcessor(t)
+
+	files := prcssr.files
+	repo := prcssr.repo.(*repository.MockPrices)
+
+	file1 := File{Path: "test1.csv"}
+	file2 := File{Path: "test2.csv"}
+
+	prcssr.wgWrite.Add(1)
+	go prcssr.saveFiles()
+
+	repo.EXPECT().ImportFile(prcssr.ctx, file1.Path).Return(nil)
+	repo.EXPECT().ImportFile(prcssr.ctx, file2.Path).Return(nil)
+
+	err := files.Put(file1)
+	assert.NoError(t, err)
+	err = files.Put(file2)
+	assert.NoError(t, err)
+
+	err = files.Close()
+	assert.NoError(t, err)
+
+	prcssr.wgWrite.Wait()
+}
+
+func TestProcessor_ProcessLines(t *testing.T) {
+	prcssr, stop := newTestLineProcessor(t)
+
+	wg := prcssr.wg
+
+	dir := prcssr.config.FilesDir
+
+	files := prcssr.files
+	repo := prcssr.repo.(*repository.MockPrices)
+
+	testutils.GenerateTestData(2, dir)
+
+	entries, err := os.ReadDir(dir)
+	assert.NoError(t, err)
+	assert.Len(t, entries, 1)
+
+	entry := entries[0]
+
+	file := File{
+		Path: fmt.Sprintf("%s/%s", dir, entry.Name()),
+		Name: entry.Name(),
+	}
+
+	f, err := os.Open(file.Path)
+	assert.NoError(t, err)
+
+	reader := csv.NewReader(f)
+	lines, err := reader.ReadAll()
+	assert.NoError(t, err)
+
+	f.Close()
+
+	prices := [][]*models.Price{
+		{
+			prcssr.toPrice(file.Path, lines[0]),
+		},
+		{
+			prcssr.toPrice(file.Path, lines[1]),
+		},
+	}
+
+	go prcssr.Process()
+
+	repo.EXPECT().Create(prcssr.ctx, prices[0]).Return(nil)
+	repo.EXPECT().Create(prcssr.ctx, prices[1]).Return(nil)
+
+	err = files.Put(file)
+	assert.NoError(t, err)
+
+	err = files.Close()
+	assert.NoError(t, err)
+	stop <- true
+	wg.Wait()
+}
+
+func TestProcessor_ProcessFiles(t *testing.T) {
+	prcssr, stop := newTestFileProcessor(t)
+
+	wg := prcssr.wg
+
+	dir := prcssr.config.FilesDir
+
+	files := prcssr.files
+	repo := prcssr.repo.(*repository.MockPrices)
+
+	testutils.GenerateTestData(2, dir)
+
+	entries, err := os.ReadDir(dir)
+	assert.NoError(t, err)
+	assert.Len(t, entries, 1)
+
+	entry := entries[0]
+
+	file := File{
+		Path: fmt.Sprintf("%s/%s", dir, entry.Name()),
+		Name: entry.Name(),
+	}
+
+	go prcssr.Process()
+
+	repo.EXPECT().ImportFile(prcssr.ctx, file.Path).Return(nil)
+
+	err = files.Put(file)
+	assert.NoError(t, err)
+
+	err = files.Close()
+	stop <- true
+	wg.Wait()
+}
