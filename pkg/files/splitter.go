e54cc8533a1c0e195ab2af332215278dbe02dab7
Mikhail Tvarkovsky <mtvarkovsky@gmail.com>
2023-08-23 09:47:48 +0200

    Add FileSplitter

---
+++ pkg/files/splitter.go
@@ -0,0 +1,144 @@
+package files
+
+import (
+	"encoding/csv"
+	"fmt"
+	"go.uber.org/zap"
+	"io"
+	"os"
+	"prices/pkg/config"
+	"sync"
+)
+
+type (
+	FileLines struct {
+		File   File
+		Lines  [][]string
+		Parent File
+	}
+
+	Splitter interface {
+		Split()
+	}
+
+	splitter struct {
+		wg         *sync.WaitGroup
+		wgInternal *sync.WaitGroup
+		config     *config.FileProcessor
+		stop       <-chan bool
+		fileLines  chan FileLines
+		files      FileQueue
+		logger     *zap.Logger
+	}
+)
+
+func NewSplitter(wg *sync.WaitGroup, logger *zap.Logger, config *config.FileProcessor, splitFiles FileQueue, stop <-chan bool) Splitter {
+	log := logger.Named("FileSplitter")
+	wgInternal := &sync.WaitGroup{}
+	s := &splitter{
+		wg:         wg,
+		wgInternal: wgInternal,
+		config:     config,
+		stop:       stop,
+		files:      splitFiles,
+		fileLines:  make(chan FileLines, config.FileSplitter.FileLinesQueueSize),
+		logger:     log,
+	}
+	return s
+}
+
+func (s *splitter) Split() {
+	s.logger.Sugar().Infof("start file splitter")
+	s.wg.Add(1)
+	go s.splitFiles()
+	for i := 0; i < s.config.FileSplitter.WorkersCount; i++ {
+		s.logger.Sugar().Infof("start file splitter worker")
+		s.wgInternal.Add(1)
+		go s.processSplits()
+	}
+	<-s.stop
+	s.logger.Sugar().Infof("stopping file splitter")
+	close(s.fileLines)
+	s.wgInternal.Wait()
+	s.logger.Sugar().Infof("stop file splitter")
+	s.wg.Done()
+}
+
+func (s *splitter) splitFiles() {
+	files, err := s.files.Data()
+	if err != nil {
+		s.logger.Sugar().Infof("can't get file queue data: (%s)", err.Error())
+		return
+	}
+	for file := range files {
+		s.wgInternal.Add(1)
+		go s.splitFile(file)
+	}
+}
+
+func (s *splitter) splitFile(file File) {
+	s.logger.Sugar().Infof("try to split file=%s", file)
+	defer s.wgInternal.Done()
+	f, err := os.Open(file.Path)
+	if err != nil {
+		s.logger.Sugar().Errorf("can't open file=%s: (%s)", file, err.Error())
+		return
+	}
+	reader := csv.NewReader(f)
+	var lines [][]string
+	counter := 0
+	for {
+		line, err := reader.Read()
+		if err != nil {
+			if err == io.EOF {
+				if len(lines) > 0 {
+					s.pushFileLines(file, lines, counter-len(lines), counter)
+				}
+				s.logger.Sugar().Infof("done reading file=%s", file)
+				_ = f.Close()
+				break
+			}
+			s.logger.Sugar().Errorf("can't read file=%s data: (%s)", file, err.Error())
+			return
+		}
+		counter += 1
+		lines = append(lines, line)
+		if len(lines)%s.config.FileSplitter.SplitByLines == 0 {
+			s.pushFileLines(file, lines, counter-len(lines), counter)
+			lines = nil
+		}
+	}
+	s.logger.Sugar().Infof("done splitting file=%s", file)
+}
+
+func (s *splitter) pushFileLines(file File, lines [][]string, start int, end int) {
+	fileLines := FileLines{
+		File: File{
+			Path: fmt.Sprintf("%s/%d_%d_%s", s.config.FilesDir, start, end, file.Name),
+		},
+		Lines:  lines,
+		Parent: file,
+	}
+	s.fileLines <- fileLines
+}
+
+func (s *splitter) processSplits() {
+	defer func() {
+		s.logger.Sugar().Infof("stop file splitter worker")
+		s.wgInternal.Done()
+	}()
+	for fl := range s.fileLines {
+		f, err := os.Create(fl.File.Path)
+		if err != nil {
+			s.logger.Sugar().Errorf("can't to create file=%s: (%s)", fl.File.Path, err.Error())
+			s.wgInternal.Done()
+			return
+		}
+		writer := csv.NewWriter(f)
+		err = writer.WriteAll(fl.Lines)
+		if err != nil {
+			s.logger.Sugar().Errorf("can't write file=%s: (%s)", fl.File.Path, err.Error())
+		}
+		f.Close()
+	}
+}
---
+++ pkg/files/splitter_test.go
@@ -0,0 +1,236 @@
+package files
+
+import (
+	"encoding/csv"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"os"
+	"prices/pkg/config"
+	"prices/pkg/testutils"
+	"sync"
+	"testing"
+)
+
+func newTestSplitter(t *testing.T) (*splitter, chan bool) {
+	dir, err := os.MkdirTemp("", "")
+	assert.NoError(t, err)
+
+	splitFiles := NewFileQueueInMem(1)
+
+	wg := &sync.WaitGroup{}
+	log := zap.NewNop()
+
+	cfg := &config.FileProcessor{
+		FilesDir:         dir,
+		MaxFileSizeBytes: 6000,
+		FileSplitter: config.FileSplitter{
+			SplitByLines:       50,
+			WorkersCount:       2,
+			FileLinesQueueSize: 2,
+		},
+	}
+
+	stop := make(chan bool)
+
+	splttr := NewSplitter(wg, log, cfg, splitFiles, stop)
+	s, ok := splttr.(*splitter)
+	assert.True(t, ok)
+
+	return s, stop
+}
+
+func TestSplitter_PushFileLines(t *testing.T) {
+	splttr, _ := newTestSplitter(t)
+	dir := splttr.config.FilesDir
+
+	file := File{
+		Path: "test",
+	}
+	lines := [][]string{
+		{"id_1", "price_1", "expirationDate_1"},
+		{"id_1", "price_2", "expirationDate_2"},
+	}
+
+	expectedLines := FileLines{
+		File: File{
+			Path: fmt.Sprintf("%s/%d_%d_%s", dir, 0, 1, file.Name),
+		},
+		Lines:  lines,
+		Parent: file,
+	}
+
+	go splttr.pushFileLines(file, lines, 0, 1)
+
+	resLines := <-splttr.fileLines
+
+	assert.Equal(t, expectedLines, resLines)
+}
+
+func TestSplitter_SplitFile(t *testing.T) {
+	splttr, _ := newTestSplitter(t)
+	dir := splttr.config.FilesDir
+	testutils.GenerateTestData(100, dir)
+
+	entries, err := os.ReadDir(dir)
+	assert.NoError(t, err)
+	assert.Len(t, entries, 1)
+
+	entry := entries[0]
+
+	file := File{
+		Path: fmt.Sprintf("%s/%s", dir, entry.Name()),
+		Name: entry.Name(),
+	}
+
+	expectedFile1 := File{
+		Path: fmt.Sprintf("%s/%d_%d_%s", dir, 0, 50, file.Name),
+	}
+	var expectedFile1Lines [][]string
+	expectedFile2 := File{
+		Path: fmt.Sprintf("%s/%d_%d_%s", dir, 50, 100, file.Name),
+	}
+	var expectedFile2Lines [][]string
+
+	f, err := os.Open(file.Path)
+	assert.NoError(t, err)
+
+	reader := csv.NewReader(f)
+	lines, err := reader.ReadAll()
+	assert.NoError(t, err)
+
+	f.Close()
+
+	expectedFile1Lines = lines[0:50]
+	expectedFile2Lines = lines[50:]
+
+	splttr.wgInternal.Add(1)
+	go splttr.splitFile(file)
+
+	lines1 := <-splttr.fileLines
+	assert.Equal(t, expectedFile1, lines1.File)
+	assert.Equal(t, file, lines1.Parent)
+	assert.Equal(t, expectedFile1Lines, lines1.Lines)
+
+	lines2 := <-splttr.fileLines
+	assert.Equal(t, expectedFile2, lines2.File)
+	assert.Equal(t, file, lines2.Parent)
+	assert.Equal(t, expectedFile2Lines, lines2.Lines)
+}
+
+func TestSplitter_ProcessSplits(t *testing.T) {
+	splttr, _ := newTestSplitter(t)
+	dir := splttr.config.FilesDir
+
+	file := File{
+		Path: fmt.Sprintf("%s/%s", dir, "test.csv"),
+		Name: "test.csv",
+	}
+
+	lines := FileLines{
+		File: File{
+			Path: fmt.Sprintf("%s/%d_%d_%s", dir, 0, 1, file.Name),
+		},
+		Lines: [][]string{
+			{"id_1", "price_1", "expirationDate_1"},
+			{"id_1", "price_2", "expirationDate_2"},
+		},
+		Parent: file,
+	}
+
+	go splttr.processSplits()
+	splttr.fileLines <- lines
+
+	for {
+		entries, err := os.ReadDir(dir)
+		assert.NoError(t, err)
+		if len(entries) == 1 {
+			break
+		}
+	}
+
+	f, err := os.Open(lines.File.Path)
+	assert.NoError(t, err)
+	reader := csv.NewReader(f)
+
+	resLines, err := reader.ReadAll()
+	assert.NoError(t, err)
+
+	assert.Equal(t, lines.Lines, resLines)
+}
+
+func TestSplitter_Split(t *testing.T) {
+	splttr, stop := newTestSplitter(t)
+	dir := splttr.config.FilesDir
+
+	files := splttr.files
+
+	testutils.GenerateTestData(100, dir)
+
+	entries, err := os.ReadDir(dir)
+	assert.NoError(t, err)
+	assert.Len(t, entries, 1)
+
+	entry := entries[0]
+
+	file := File{
+		Path: fmt.Sprintf("%s/%s", dir, entry.Name()),
+		Name: entry.Name(),
+	}
+
+	expectedFile1 := File{
+		Path: fmt.Sprintf("%s/%d_%d_%s", dir, 0, 50, file.Name),
+	}
+	var expectedFile1Lines [][]string
+	expectedFile2 := File{
+		Path: fmt.Sprintf("%s/%d_%d_%s", dir, 50, 100, file.Name),
+	}
+	var expectedFile2Lines [][]string
+
+	f, err := os.Open(file.Path)
+	assert.NoError(t, err)
+
+	reader := csv.NewReader(f)
+	lines, err := reader.ReadAll()
+	assert.NoError(t, err)
+
+	f.Close()
+
+	expectedFile1Lines = lines[0:50]
+	expectedFile2Lines = lines[50:]
+
+	go splttr.Split()
+
+	err = files.Put(file)
+	assert.NoError(t, err)
+
+	for {
+		entries, err := os.ReadDir(dir)
+		assert.NoError(t, err)
+		if len(entries) == 3 {
+			break
+		}
+	}
+
+	stop <- true
+	err = files.Close()
+	assert.NoError(t, err)
+
+	_, open := <-splttr.fileLines
+	assert.False(t, open)
+
+	f1, err := os.Open(expectedFile1.Path)
+	reader = csv.NewReader(f1)
+	f1Lines, err := reader.ReadAll()
+	assert.Equal(t, expectedFile1Lines, f1Lines)
+	f1.Close()
+
+	f2, err := os.Open(expectedFile2.Path)
+	reader = csv.NewReader(f2)
+	f2Lines, err := reader.ReadAll()
+	assert.Equal(t, expectedFile2Lines, f2Lines)
+	f2.Close()
+
+	_, err = os.Stat(file.Path)
+	assert.NoError(t, err)
+}
